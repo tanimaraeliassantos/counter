e59123b8986af39404f41aad6135561881bd77d8
Tanimara Santos <36615712+anthropovixen@users.noreply.github.com>
2022-03-04 11:23:40 +1100

    Create app

---
+++ src/Count.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Count(props) {
+    console.log("count component rendered")
+  return (
+    <div className="counter--count">
+      <h1>{props.number}</h1>
+    </div>
+  );
+}
